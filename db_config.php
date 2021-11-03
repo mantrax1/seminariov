e3bdd7bf6e0b2e25860f17093c85fc0f6b0cd7b0
mantrax1 <77859322+mantrax1@users.noreply.github.com>
2021-11-03 15:45:01 -0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "ygjucnoiajhyvy"); // Usuario
-define('DB_PASSWORD', "6b888453ffcfae1d59d6a6cf8704e2105faf6575a9ecbdc5182775563cb0f2c8"); // Password
-define('DB_DATABASE', "d26tcjsig77cut"); // Nombre de la base de datos
-define('DB_SERVER', "ec2-52-2-118-38.compute-1.amazonaws.com"); // db server
+define('DB_USER', " "); // Usuario
+define('DB_PASSWORD', " "); // Password
+define('DB_DATABASE', " "); // Nombre de la base de datos
+define('DB_SERVER', " "); // db server
 ?>
